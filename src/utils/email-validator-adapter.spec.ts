339196cc621ba8953391744e06a3fd81bf823094
KelpGF <kelpgf@gmail.com>
2022-12-17 19:35:17 -0300

    refactor: move sut creation to factory helper method

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -1,5 +1,7 @@
-import { EmailValidatorAdapter } from './email-validator-adapter'
 import validator from 'validator'
+
+import { EmailValidator } from '../presentation/protocols/email-validator'
+import { EmailValidatorAdapter } from './email-validator-adapter'
 
 jest.mock('validator', () => ({
   isEmail (): boolean {
@@ -7,25 +9,29 @@ jest.mock('validator', () => ({
   }
 }))
 
+const makeSut = (): EmailValidator => {
+  return new EmailValidatorAdapter()
+}
+
 describe('EmailValidator Adapter', () => {
   test('Should returns false if validator returns false', () => {
     jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
 
-    const sut = new EmailValidatorAdapter()
+    const sut = makeSut()
     const isValid = sut.isValid('invalid_email')
 
     expect(isValid).toBe(false)
   })
 
   test('Should returns true if validator returns true', () => {
-    const sut = new EmailValidatorAdapter()
+    const sut = makeSut()
     const isValid = sut.isValid('valid_email@gmail.com')
 
     expect(isValid).toBe(true)
   })
 
   test('Should call validator with correct email', () => {
-    const sut = new EmailValidatorAdapter()
+    const sut = makeSut()
     const isEmailSpy = jest.spyOn(validator, 'isEmail')
 
     sut.isValid('any_email@gmail.com')
