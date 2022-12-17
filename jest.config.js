3fe07c55f314aa2748e4d0936bcb7f2a3a40dbbc
KelpGF <kelpgf@gmail.com>
2022-12-17 11:42:07 -0300

    chore: remove lintstage, refactor husky and ajust jest

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
-  coverageDIrectory: 'coverage',
+  coverageDirectory: 'coverage',
   testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest'
