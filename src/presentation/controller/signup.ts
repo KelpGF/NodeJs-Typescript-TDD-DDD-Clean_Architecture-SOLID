0d482108690667f13f444d09695ea4b4dc932e5d
KelpGF <kelpgf@gmail.com>
2022-12-17 17:02:00 -0300

    feat: ensure SingUpController return 400 if password confirmation fails

--- src/presentation/controller/signup.ts
+++ src/presentation/controller/signup.ts
@@ -13,10 +13,10 @@ export class SignUpController implements Controller {
       const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
 
       for (const field of requiredFields) {
-        if (!httpRequest.body[field]) {
-          return badRequest(new MissingParamError(field))
-        }
+        if (!httpRequest.body[field]) { return badRequest(new MissingParamError(field)) }
       }
+
+      if (httpRequest.body.password !== httpRequest.body.passwordConfirmation) { return badRequest(new InvalidParamError('passwordConfirmation')) }
 
       const emailIsValid = this.emailValidator.isValid(httpRequest.body.email)
 
