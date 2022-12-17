11b2d7b2a5a751a4e5f876ec6178f39218e30cb7
KelpGF <kelpgf@gmail.com>
2022-12-17 14:24:15 -0300

    test: ensure SignUpController returns 400 if no password is provided

--- src/presentation/controller/singup.spec.ts
+++ src/presentation/controller/singup.spec.ts
@@ -2,7 +2,7 @@ import { MissingParamError } from '../errors/missing-params-error'
 import { SignUpController } from './signup'
 
 describe('SignUp Controller', () => {
-  test('Should return status code if no name provide', () => {
+  test('Should return status code if no name provided', () => {
     const sut = new SignUpController()
 
     const httpRequest = {
@@ -19,7 +19,7 @@ describe('SignUp Controller', () => {
     expect(httpResponse.body).toEqual(new MissingParamError('name'))
   })
 
-  test('Should return status code if no email provide', () => {
+  test('Should return status code if no email provided', () => {
     const sut = new SignUpController()
 
     const httpRequest = {
@@ -34,5 +34,22 @@ describe('SignUp Controller', () => {
 
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('email'))
+  })
+
+  test('Should return status code if no password provided', () => {
+    const sut = new SignUpController()
+
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        passwordConfirmation: 'any_pass'
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('password'))
   })
 })
