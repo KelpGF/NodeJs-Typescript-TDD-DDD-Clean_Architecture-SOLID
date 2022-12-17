3da7a9033e389b32b2441dd7a4e854a422547019
KelpGF <kelpgf@gmail.com>
2022-12-17 18:03:26 -0300

    refactor: change import

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -1,5 +1,5 @@
-import { InternalServerError } from '../errors/internal-server-error'
-import { HttpResponse } from '../protocols/http'
+import { InternalServerError } from '../errors'
+import { HttpResponse } from '../protocols'
 
 export const badRequest = (error: Error): HttpResponse => ({ statusCode: 400, body: error })
 export const internalServerError = (): HttpResponse => ({ statusCode: 500, body: new InternalServerError() })
