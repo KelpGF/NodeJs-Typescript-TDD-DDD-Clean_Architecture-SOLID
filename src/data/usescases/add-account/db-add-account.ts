9448b8a43d61a2adfb812332fc3c80678ed4818e
KelpGF <kelpgf@gmail.com>
2022-12-19 20:04:45 -0300

    refactor: save returned account in a variable

--- src/data/usescases/add-account/db-add-account.ts
+++ src/data/usescases/add-account/db-add-account.ts
@@ -11,10 +11,7 @@ export class DBAddAccount implements AddAccount {
 
   async add (accountData: AddAccountModel): Promise<AccountModel> {
     const hashedPassword = await this.encrypter.encrypt(accountData.password)
-
-    console.log(hashedPassword)
-    await this.addAccountRepository.add(Object.assign({}, accountData, { password: hashedPassword }))
-
-    return await Promise.resolve({ email: accountData.email, name: accountData.name, id: 'valid_id', password: hashedPassword })
+    const account = await this.addAccountRepository.add(Object.assign({}, accountData, { password: hashedPassword }))
+    return account
   }
 }
