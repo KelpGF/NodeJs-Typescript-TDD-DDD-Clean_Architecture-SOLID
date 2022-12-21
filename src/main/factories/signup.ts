83aa531ccd38e1f0b4ca02a4ff1163c350857dd2
KelpGF <kelpgf@gmail.com>
2022-12-20 23:48:43 -0300

    feat: add SignUp Factory

---
+++ src/main/factories/signup.ts
@@ -0,0 +1,14 @@
+import { DBAddAccount } from '../../data/usescases/add-account/db-add-account'
+import { BcryptAdapter } from '../../infra/cryptography/bcrypt-adapter'
+import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
+import { SignUpController } from '../../presentation/controller/signup/signup'
+import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
+
+export const makeSignUpController = (): SignUpController => {
+  const salt = 12
+  const emailValidatorAdapter = new EmailValidatorAdapter()
+  const bcryptAdapter = new BcryptAdapter(salt)
+  const accountMongoRepository = new AccountMongoRepository()
+  const addAccount = new DBAddAccount(bcryptAdapter, accountMongoRepository)
+  return new SignUpController(emailValidatorAdapter, addAccount)
+}
