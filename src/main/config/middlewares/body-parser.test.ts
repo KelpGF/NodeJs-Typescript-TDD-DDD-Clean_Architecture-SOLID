aa82601a8bcbeb412d37af5c538b4c5ecc7e0513
KelpGF <kelpgf@gmail.com>
2022-12-20 22:43:23 -0300

    test: ensure request parser body as json

---
+++ src/main/config/middlewares/body-parser.test.ts
@@ -0,0 +1,12 @@
+import request from 'supertest'
+import app from '../app'
+
+describe('Body Parser Middleware', () => {
+  test('Should parser body as json', async () => {
+    app.post('/test_body_parser', (req, res) => {
+      res.send(req.body)
+    })
+
+    await request(app).post('/test_body_parser').send({ name: 'Kelvin' }).expect({ name: 'Kelvin' })
+  })
+})
