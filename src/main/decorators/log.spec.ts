061a72bd0e1796eb9fe9a8d24fb8643d0c37fd91
KelpGF <kelpgf@gmail.com>
2022-12-24 17:05:59 -0300

    refactor: move sut and stub creation to a factories methods

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -2,16 +2,30 @@ import { Controller, HttpRequest, HttpResponse } from '../../presentation/protoc
 import { LogControllerDecorator } from './log'
 
 describe('Log Controller Decorator', () => {
-  test('Should LogControllerDecorator calls controller handle with correct value', async () => {
+  interface SutTypes {
+    sut: Controller
+    stubController: Controller
+  }
+
+  const makeStubController = (): Controller => {
     class StubController implements Controller {
       async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
         return await Promise.resolve(null as unknown as HttpResponse)
       }
     }
 
-    const stubController = new StubController()
-    const spyStubHandle = jest.spyOn(stubController, 'handle')
+    return new StubController()
+  }
+  const makeSut = (): SutTypes => {
+    const stubController = makeStubController()
     const sut = new LogControllerDecorator(stubController)
+
+    return { sut, stubController }
+  }
+
+  test('Should LogControllerDecorator calls controller handle with correct value', async () => {
+    const { sut, stubController } = makeSut()
+    const spyStubHandle = jest.spyOn(stubController, 'handle')
     const httpRequest: HttpRequest = {
       body: {
         name: 'any_name',
