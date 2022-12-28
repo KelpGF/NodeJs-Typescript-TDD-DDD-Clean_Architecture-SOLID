6f21731b85241fabfe90ff1e85231b9df0fe9ea8
KelpGF <kelpgf@gmail.com>
2022-12-27 22:02:50 -0300

    test: ensure RequiredFieldValidation returns a MissingParamError if validation fails

---
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -0,0 +1,11 @@
+import { MissingParamError } from '../../errors'
+import { RequiredFieldValidation } from './required-field-validation'
+
+describe('RequiredField Validation', () => {
+  test('Should return a MissingParamError if validation fails', () => {
+    const sut = new RequiredFieldValidation('any_field')
+    const error = sut.validate({})
+
+    expect(error).toEqual(new MissingParamError('any_field'))
+  })
+})
