9a6a1fd1a9bb48a1d52f4bca7954238b127ae1cc
Kelvin Gomes <kelvin.gomes@tallos.com.br>
2022-12-29 16:00:48 -0300

    feat: ensure DbAuthentication calls LoadAccountByEmailRepository with correct email

---
+++ src/data/usescases/authentication/db-authentication.ts
@@ -0,0 +1,16 @@
+import { Authentication, AuthenticationModel } from '../../../domain/usecases/authentication'
+import { LoadAccountByEmailRepository } from '../../protocols/load-account-by-email-repository'
+
+export class DbAuthentication implements Authentication {
+  private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
+
+  constructor (loadAccountByEmailRepository: LoadAccountByEmailRepository) {
+    this.loadAccountByEmailRepository = loadAccountByEmailRepository
+  }
+
+  async auth (authentication: AuthenticationModel): Promise<string | null> {
+    await this.loadAccountByEmailRepository.load(authentication.email)
+
+    return null
+  }
+}
