7fa78acfccb0ef91ca557880bda10ebd9c8ace51
KelpGF <kelpgf@gmail.com>
2022-12-29 20:15:40 -0300

    feat: ensure AuthenticationModel calls TokenGenerator with correct value

--- src/data/usescases/authentication/db-authentication.ts
+++ src/data/usescases/authentication/db-authentication.ts
@@ -1,14 +1,21 @@
 import { Authentication, AuthenticationModel } from '../../../domain/usecases/authentication'
 import { HashComparer } from '../../protocols/cryptography/hash-comparer'
 import { LoadAccountByEmailRepository } from '../../protocols/db/load-account-by-email-repository'
+import { TokenGenerator } from '../../protocols/token/token-generator'
 
 export class DbAuthentication implements Authentication {
   private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
   private readonly hashComparer: HashComparer
+  private readonly tokenGenerator: TokenGenerator
 
-  constructor (loadAccountByEmailRepository: LoadAccountByEmailRepository, hashComparer: HashComparer) {
+  constructor (
+    loadAccountByEmailRepository: LoadAccountByEmailRepository,
+    hashComparer: HashComparer,
+    tokenGenerator: TokenGenerator
+  ) {
     this.loadAccountByEmailRepository = loadAccountByEmailRepository
     this.hashComparer = hashComparer
+    this.tokenGenerator = tokenGenerator
   }
 
   async auth (authentication: AuthenticationModel): Promise<string | null> {
@@ -16,7 +23,9 @@ export class DbAuthentication implements Authentication {
     if (account) {
       const matchPasswords = await this.hashComparer.compare(authentication.password, account.password)
       if (matchPasswords) {
-        return 'token'
+        const accessToken = await this.tokenGenerator.generate(account.id)
+
+        return accessToken
       }
     }
 
