771b873c854b16008727dca231c80bb4550cad64
KelpGF <kelpgf@gmail.com>
2022-12-29 20:21:52 -0300

    feat: ensure DbAuthentication return accessToken if success

--- src/data/usescases/authentication/db-authentication.ts
+++ src/data/usescases/authentication/db-authentication.ts
@@ -21,11 +21,9 @@ export class DbAuthentication implements Authentication {
   async auth (authentication: AuthenticationModel): Promise<string | null> {
     const account = await this.loadAccountByEmailRepository.load(authentication.email)
     if (account) {
-      const matchPasswords = await this.hashComparer.compare(authentication.password, account.password)
-      if (matchPasswords) {
-        const accessToken = await this.tokenGenerator.generate(account.id)
-
-        return accessToken
+      const isValid = await this.hashComparer.compare(authentication.password, account.password)
+      if (isValid) {
+        return await this.tokenGenerator.generate(account.id)
       }
     }
 
