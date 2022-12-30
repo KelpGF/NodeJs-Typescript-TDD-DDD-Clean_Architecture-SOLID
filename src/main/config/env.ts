285142b3d5875f5115eb8aeeb571d0d5f6244224
Kelvin Gomes <kelvin.gomes@tallos.com.br>
2022-12-30 19:46:35 -0300

    feat: add Login route

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
   port: process.env.PORT || 5050,
   mongoConnectUrl: process.env.MONGO_CONNECT_URL || 'mongodb+srv://root:123@mongokelvin.5kpqp.mongodb.net/test',
-  jwtSecret: process.env.JWT_SECRET || '@(MDQS!@SEK)#!KE'
+  jwtSecret: process.env.JWT_SECRET || '@(MD!@SEK#!qc'
 }
--- src/main/routes/login-routes.ts
+++ src/main/routes/login-routes.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express'
 import { expressRoutesAdapter } from '../adapters/express/express-routes-adapter'
+import { makeLoginController } from '../factories/login/login-factory'
 import { makeSignUpController } from '../factories/signup/signup-factory'
 
 export default (router: Router): void => {
   router.post('/signup', expressRoutesAdapter(makeSignUpController()))
+  router.post('/login', expressRoutesAdapter(makeLoginController()))
 }
