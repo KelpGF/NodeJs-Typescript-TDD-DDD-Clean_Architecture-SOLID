3280ad2906e8efa68316c1a379ae38d699778ccf
KelpGF <kelpgf@gmail.com>
2022-12-29 22:00:27 -0300

    test: ensure JwtAdapter throws if sign throws

--- src/infra/cryptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/cryptography/jwt-adapter/jwt-adapter.spec.ts
@@ -15,9 +15,16 @@ describe('Jwt Adapter', () => {
     expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, 'secret_key')
   })
 
-  test('Should return a token on sign success', async () => {
+  test('Should return a token when sign success', async () => {
     const sut = makeSut()
     const accessToken = await sut.encrypt('any_id')
     expect(accessToken).toBe('any_token')
+  })
+
+  test('Should throw if sign throws', async () => {
+    const sut = makeSut()
+    jest.spyOn(jwt, 'sign').mockImplementationOnce(() => { throw new Error() })
+    const promise = sut.encrypt('any_id')
+    await expect(promise).rejects.toThrow()
   })
 })
