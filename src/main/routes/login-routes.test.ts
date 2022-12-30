82b6ae4e5cffe9ff1ef2f301b2ce3ad04b2e73ba
Kelvin Gomes <kelvin.gomes@tallos.com.br>
2022-12-30 19:27:54 -0300

    refactor: rename signup routes with login routes

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -2,7 +2,7 @@ import request from 'supertest'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import app from '../config/app'
 
-describe('SignUp Routes', () => {
+describe('Login Routes', () => {
   beforeAll(async () => {
     await MongoHelper.connect(String(process.env.MONGO_URL))
   })
@@ -16,15 +16,17 @@ describe('SignUp Routes', () => {
     await MongoHelper.disconnect()
   })
 
-  test('Should return an account on success', async () => {
-    await request(app)
-      .post('/api/signup')
-      .send({
-        name: 'Kelvin',
-        email: 'kelvin.gomes.fernandes@email.com.br',
-        password: 'qwe123',
-        passwordConfirmation: 'qwe123'
-      })
-      .expect(200)
+  describe('POST /signup', () => {
+    test('Should return 200 on signup', async () => {
+      await request(app)
+        .post('/api/signup')
+        .send({
+          name: 'Kelvin',
+          email: 'kelvin.gomes.fernandes@email.com.br',
+          password: 'qwe123',
+          passwordConfirmation: 'qwe123'
+        })
+        .expect(200)
+    })
   })
 })
