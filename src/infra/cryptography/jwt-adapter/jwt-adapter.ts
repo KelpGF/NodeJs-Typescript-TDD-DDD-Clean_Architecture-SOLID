4c4a9f06aac2717c363bd0beaf63de3a76271875
KelpGF <kelpgf@gmail.com>
2022-12-29 21:55:32 -0300

    feat: ensure JwtAdapter returns a token when sign success

--- src/infra/cryptography/jwt-adapter/jwt-adapter.ts
+++ src/infra/cryptography/jwt-adapter/jwt-adapter.ts
@@ -9,8 +9,6 @@ export class JwtAdapter implements Encrypter {
   }
 
   async encrypt (value: string): Promise<string> {
-    jwt.sign({ id: value }, this.secretKey)
-
-    return ''
+    return jwt.sign({ id: value }, this.secretKey)
   }
 }
