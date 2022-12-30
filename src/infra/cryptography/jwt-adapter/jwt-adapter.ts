9621346022eca195a77bd1667a46e36005f84991
KelpGF <kelpgf@gmail.com>
2022-12-29 21:51:28 -0300

    feat: ensure JwtAdapter calls sign with correct values

---
+++ src/infra/cryptography/jwt-adapter/jwt-adapter.ts
@@ -0,0 +1,16 @@
+import jwt from 'jsonwebtoken'
+import { Encrypter } from '../../../data/protocols/cryptography/encrypter'
+
+export class JwtAdapter implements Encrypter {
+  private readonly secretKey: string
+
+  constructor (secretKey: string) {
+    this.secretKey = secretKey
+  }
+
+  async encrypt (value: string): Promise<string> {
+    jwt.sign({ id: value }, this.secretKey)
+
+    return ''
+  }
+}
