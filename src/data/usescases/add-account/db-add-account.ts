8ac18d5f0a4fb53240a5cb500221d61128ab03a2
Kelvin Gomes <kelvin.gomes@tallos.com.br>
2023-01-04 19:50:19 -0300

    feat: ensure DBAddAccount calls FindAccountByEmailRepository if correct email

--- src/data/usescases/add-account/db-add-account.ts
+++ src/data/usescases/add-account/db-add-account.ts
@@ -1,12 +1,16 @@
+import { FindAccountByEmailRepository } from '../authentication/db-authentication-protocols'
 import { AccountModel, AddAccount, AddAccountModel, AddAccountRepository, Hasher } from './db-add-account-protocols'
 
 export class DBAddAccount implements AddAccount {
   constructor (
     private readonly hasher: Hasher,
-    private readonly addAccountRepository: AddAccountRepository
+    private readonly addAccountRepository: AddAccountRepository,
+    private readonly findAccountByEmailRepository: FindAccountByEmailRepository
   ) {}
 
   async add (accountData: AddAccountModel): Promise<AccountModel> {
+    await this.findAccountByEmailRepository.findByEmail(accountData.email)
+
     const hashedPassword = await this.hasher.hash(accountData.password)
     const account = await this.addAccountRepository.add(Object.assign({}, accountData, { password: hashedPassword }))
     return account
