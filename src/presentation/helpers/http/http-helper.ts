ad77f087d00dc32ce1bade42bec38df6d82a280d
KelpGF <kelpgf@gmail.com>
2023-01-05 20:04:17 -0300

    refactor: change status code from badReq and Forbidden

--- src/presentation/helpers/http/http-helper.ts
+++ src/presentation/helpers/http/http-helper.ts
@@ -3,7 +3,7 @@ import { HttpResponse } from '../../protocols'
 
 export const ok = (data: any): HttpResponse => ({ statusCode: 200, body: data })
 export const noContent = (): HttpResponse => ({ statusCode: 200, body: null })
-export const badRequest = (error: Error): HttpResponse => ({ statusCode: 403, body: error })
+export const badRequest = (error: Error): HttpResponse => ({ statusCode: 400, body: error })
 export const unauthorized = (): HttpResponse => ({ statusCode: 401, body: new UnauthorizedError() })
-export const forbidden = (error: Error): HttpResponse => ({ statusCode: 400, body: error })
+export const forbidden = (error: Error): HttpResponse => ({ statusCode: 403, body: error })
 export const internalServerError = (error: Error): HttpResponse => ({ statusCode: 500, body: new InternalServerError(String(error.stack)) })
