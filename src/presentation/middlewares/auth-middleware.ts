fd65563cd325d70b89e17139a710420da3f207cd
KelpGF <kelpgf@gmail.com>
2023-01-06 20:30:55 -0300

    feat: ensure AuthMiddleware calls FindAccountByToken correct role

--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -4,13 +4,16 @@ import { HttpRequest, HttpResponse, Middleware } from '../protocols'
 import { FindAccountByToken } from '../../domain/usecases/find-account-by-token'
 
 export class AuthMiddleware implements Middleware {
-  constructor (private readonly findAccountByToken: FindAccountByToken) {}
+  constructor (
+    private readonly findAccountByToken: FindAccountByToken,
+    private readonly role: string
+  ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const accessToken = httpRequest.headers?.['x-access-token']
       if (accessToken) {
-        const account = await this.findAccountByToken.find(accessToken)
+        const account = await this.findAccountByToken.find(accessToken, this.role)
 
         if (account) return ok({ accountId: account.id })
       }
