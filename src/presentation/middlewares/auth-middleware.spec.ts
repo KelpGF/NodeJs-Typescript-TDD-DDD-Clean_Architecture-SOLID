d2460a7371cf8945e313ce4e0739ed4bf6d7c16a
KelpGF <kelpgf@gmail.com>
2023-01-06 20:13:10 -0300

    test: ensure AuthMiddleware calls FindAccountByToken correct accessToken

--- src/presentation/middlewares/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth-middleware.spec.ts
@@ -1,11 +1,51 @@
+import { AccountModel } from '../../domain/models/account'
+import { FindAccountByToken } from '../../domain/usecases/find-account-by-token'
 import { AccessDeniedError } from '../errors/access-denied-error'
 import { forbidden } from '../helpers/http/http-helper'
+import { HttpRequest } from '../protocols'
 import { AuthMiddleware } from './auth-middleware'
+
+const makeFakeAccount = (): AccountModel => ({
+  id: 'valid_id',
+  name: 'valid_name',
+  email: 'valid_email@mail.com',
+  password: 'valid_pass'
+})
+const makeFindAccountByTokenStub = (): FindAccountByToken => {
+  class FindAccountByTokenStub implements FindAccountByToken {
+    async find (accessToken: string): Promise<AccountModel> {
+      return makeFakeAccount()
+    }
+  }
+
+  return new FindAccountByTokenStub()
+}
+const makeFakeRequest = (): HttpRequest => ({
+  headers: { 'x-access-token': 'any_token' }
+})
+interface SutTypes {
+  sut: AuthMiddleware
+  findAccountByTokenStub: FindAccountByToken
+}
+const makeSut = (): SutTypes => {
+  const findAccountByTokenStub = makeFindAccountByTokenStub()
+  const sut = new AuthMiddleware(findAccountByTokenStub)
+
+  return { sut, findAccountByTokenStub }
+}
 
 describe('Auth Middleware', () => {
   test('Should return 403 if no x-access-token exists in headers', async () => {
-    const sut = new AuthMiddleware()
+    const { sut } = makeSut()
     const httpResponse = await sut.handle({})
     expect(httpResponse).toEqual(forbidden(new AccessDeniedError()))
+  })
+
+  test('Should call FindAccountByToken correct accessToken', async () => {
+    const { sut, findAccountByTokenStub } = makeSut()
+    const findSpy = jest.spyOn(findAccountByTokenStub, 'find')
+    const httpRequest = makeFakeRequest()
+    await sut.handle(httpRequest)
+    expect(findSpy).toHaveBeenCalledWith(httpRequest.headers['x-access-token'])
   })
 })
