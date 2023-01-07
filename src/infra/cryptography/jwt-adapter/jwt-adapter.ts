48856d0b7c4768916f32cea455ddd8620ad53dae
KelpGF <kelpgf@gmail.com>
2023-01-06 22:33:21 -0300

    feat: ensure JwtAdapter return a value on verify success

--- src/infra/cryptography/jwt-adapter/jwt-adapter.ts
+++ src/infra/cryptography/jwt-adapter/jwt-adapter.ts
@@ -9,8 +9,8 @@ export class JwtAdapter implements Encrypter, Decrypter {
     return jwt.sign({ id: value }, this.secretKey)
   }
 
-  async decrypt (value: string): Promise<string | null> {
-    jwt.verify(value, this.secretKey)
-    return null
+  async decrypt (token: string): Promise<string | null> {
+    const value: any = jwt.verify(token, this.secretKey)
+    return value
   }
 }
