94895c6d9b05b3dcc1504d6f3931db1a2c1237f3
KelpGF <kelpgf@gmail.com>
2023-01-06 22:35:57 -0300

    test: ensure JwtAdapter throw if verify throws

--- src/infra/cryptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/cryptography/jwt-adapter/jwt-adapter.spec.ts
@@ -44,5 +44,12 @@ describe('Jwt Adapter', () => {
       const accessToken = await sut.decrypt('any_token')
       expect(accessToken).toBe('any_value')
     })
+
+    test('Should throw if verify throws', async () => {
+      const sut = makeSut()
+      jest.spyOn(jwt, 'verify').mockImplementationOnce(() => { throw new Error() })
+      const promise = sut.decrypt('any_token')
+      await expect(promise).rejects.toThrow()
+    })
   })
 })
