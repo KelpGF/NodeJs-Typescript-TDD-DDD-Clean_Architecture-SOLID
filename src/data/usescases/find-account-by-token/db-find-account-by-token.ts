e52bf99ff36dae7e831405995652a4f93e75c132
KelpGF <kelpgf@gmail.com>
2023-01-07 17:06:01 -0300

    fix: ensure DbFindAccountByToken calls findAccountByTokenRepository with correct accessToken

--- src/data/usescases/find-account-by-token/db-find-account-by-token.ts
+++ src/data/usescases/find-account-by-token/db-find-account-by-token.ts
@@ -9,7 +9,7 @@ export class DbFindAccountByToken implements FindAccountByToken {
   async find (accessToken: string, role?: string | undefined): Promise<AccountModel | null> {
     const token = await this.decrypter.decrypt(accessToken)
     if (token) {
-      const account = await this.findAccountByTokenRepository.findByToken(token, role)
+      const account = await this.findAccountByTokenRepository.findByToken(accessToken, role)
       if (account) return account
     }
     return null
