6530f02e4b93286e599960af8680f943db5b93cf
KelpGF <kelpgf@gmail.com>
2023-01-06 21:37:30 -0300

    refactor: rename data layer protocol file

--- src/data/protocols/db/account/index.ts
+++ src/data/protocols/db/account/index.ts
@@ -1,3 +1,3 @@
 export * from './insert-account-repository'
-export * from './load-account-by-email-repository'
+export * from './find-account-by-email-repository'
 export * from './update-access-token-repository'
