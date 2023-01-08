54aceb767a5df3bb7cf8dfcf7c8456fdbd0b2b25
KelpGF <kelpgf@gmail.com>
2023-01-08 14:18:27 -0300

    feat: ensure SurveyMongoRepository find all on success

--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -1,9 +1,17 @@
 import { InsertSurveyModel, InsertSurveyRepository } from '../../../../data/usescases/add-survey/db-add-survey-protocols'
+import { FindSurveysRepository, SurveyModel } from '../../../../data/usescases/list/db-list-surveys-protocols'
 import { MongoHelper } from '../helpers/mongo-helper'
 
-export class SurveyMongoRepository implements InsertSurveyRepository {
+export class SurveyMongoRepository implements InsertSurveyRepository, FindSurveysRepository {
   async insert (surveyData: InsertSurveyModel): Promise<void> {
     const surveyCollection = await MongoHelper.getCollection('surveys')
     await surveyCollection.insertOne(surveyData)
+  }
+
+  async findAll (): Promise<SurveyModel[]> {
+    const surveyCollection = await MongoHelper.getCollection('surveys')
+    const surveys = await surveyCollection.find().toArray()
+
+    return surveys.map((survey) => MongoHelper.map(survey))
   }
 }
