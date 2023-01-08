8f7c832d38bb512a02b043ed09da67577d4fbbc2
KelpGF <kelpgf@gmail.com>
2023-01-08 13:01:51 -0300

    test: ensure ListSurveyController calls ListSurveysUseCase

---
+++ src/presentation/controller/survey/list-surveys/list-surveys-controller.spec.ts
@@ -0,0 +1,56 @@
+import { ListSurveyController } from './list-surveys-controller'
+import { ListSurvey, SurveyModel } from './list-surveys-controller-protocols'
+import MockDate from 'mockdate'
+
+const makeFakeSurveys = (): SurveyModel[] => ([
+  {
+    id: 'any_id',
+    question: 'any_question',
+    answers: [{ answer: 'any_answer', image: 'any_image' }],
+    date: new Date()
+  },
+  {
+    id: 'other_id',
+    question: 'other_question',
+    answers: [{ answer: 'other_answer' }],
+    date: new Date()
+  }
+])
+
+const makeListSurveyStub = (): ListSurvey => {
+  class ListSurveyStub implements ListSurvey {
+    async list (): Promise<SurveyModel[]> {
+      return makeFakeSurveys()
+    }
+  }
+
+  return new ListSurveyStub()
+}
+
+interface SutTypes {
+  sut: ListSurveyController
+  listSurveyStub: ListSurvey
+}
+const makeSut = (): SutTypes => {
+  const listSurveyStub = makeListSurveyStub()
+  const sut = new ListSurveyController(listSurveyStub)
+
+  return { sut, listSurveyStub }
+}
+
+describe('ListSurveys Controller', () => {
+  beforeAll(() => {
+    MockDate.set(new Date())
+  })
+
+  afterAll(() => {
+    MockDate.reset()
+  })
+
+  test('Should call ListSurveys', async () => {
+    const { sut, listSurveyStub } = makeSut()
+    const listSpy = jest.spyOn(listSurveyStub, 'list')
+    await sut.handle({})
+    expect(listSpy).toHaveBeenCalled()
+  })
+})
