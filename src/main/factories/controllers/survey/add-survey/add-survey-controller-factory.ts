b40b58e9d71da2c9eac96f8629c5d4a5a99f7751
KelpGF <kelpgf@gmail.com>
2023-01-09 19:09:16 -0300

    refactor: rename dbAddSurveyFactory to makeDbAddSurvey

--- src/main/factories/controllers/survey/add-survey/add-survey-controller-factory.ts
+++ src/main/factories/controllers/survey/add-survey/add-survey-controller-factory.ts
@@ -1,10 +1,10 @@
 import { makeAddSurveyValidation } from './add-survey-validation-factory'
-import { dbAddSurveyFactory } from '../../../usecases/survey/add-survey/db-add-survey-factory'
+import { makeDbAddSurvey } from '../../../usecases/survey/add-survey/db-add-survey-factory'
 import { Controller } from '../../../../../presentation/protocols'
 import { AddSurveyController } from '../../../../../presentation/controller/survey/add/add-survey-controller'
 import { makeLogControllerDecoratorFactory } from '../../../decorators/log-controller-decorator-factory'
 
 export const makeAddSurveyController = (): Controller => {
-  const addSurveyController = new AddSurveyController(makeAddSurveyValidation(), dbAddSurveyFactory())
+  const addSurveyController = new AddSurveyController(makeAddSurveyValidation(), makeDbAddSurvey())
   return makeLogControllerDecoratorFactory(addSurveyController)
 }
--- src/main/factories/usecases/survey/add-survey/db-add-survey-factory.ts
+++ src/main/factories/usecases/survey/add-survey/db-add-survey-factory.ts
@@ -2,7 +2,7 @@ import { DbAddSurvey } from '../../../../../data/usescases/add-survey/db-add-sur
 import { AddSurvey } from '../../../../../domain/usecases/add-survey'
 import { SurveyMongoRepository } from '../../../../../infra/db/mongodb/survey/survey-mongo-repository'
 
-export const dbAddSurveyFactory = (): AddSurvey => {
+export const makeDbAddSurvey = (): AddSurvey => {
   const insertSurveyRepository = new SurveyMongoRepository()
   return new DbAddSurvey(insertSurveyRepository)
 }
