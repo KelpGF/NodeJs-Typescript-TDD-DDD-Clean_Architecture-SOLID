78692814150eeeae0f1d3cab8438c878f9907079
KelpGF <kelpgf@gmail.com>
2023-01-09 19:12:17 -0300

    test: ensure list surveys route returns 403 if no access token is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -64,4 +64,12 @@ describe('Survey Routes', () => {
         .expect(204)
     })
   })
+
+  describe('GET /surveys', () => {
+    test('Should return 403 on list survey without accessToken', async () => {
+      await request(app)
+        .get('/api/surveys')
+        .expect(403)
+    })
+  })
 })
