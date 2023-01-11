b01ace9bad94def56ee510e2daade00d7ed0398d
KelpGF <kelpgf@gmail.com>
2023-01-10 22:36:34 -0300

    test: ensure SurveyResultMongoRepository updates surveyResult if its not new

--- src/infra/db/mongodb/survey-result/survey-result-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-repository.spec.ts
@@ -1,5 +1,5 @@
 import { SaveSurveyResultModel } from '@/domain/usecases/save-survey-result'
-import { Collection } from 'mongodb'
+import { Collection, ObjectId } from 'mongodb'
 import { MongoHelper } from '../helpers/mongo-helper'
 import { SurveyResultMongoRepository } from './survey-result-mongo-repository'
 
@@ -10,7 +10,7 @@ let accountCollection: Collection
 const makeFakeSurveyId = async (): Promise<string> => {
   const { insertedId } = await surveyCollection.insertOne({
     question: 'any_question',
-    answers: [{ answer: 'any_answer', image: 'any_image' }],
+    answers: [{ answer: 'any_answer', image: 'any_image' }, { answer: 'other_answer' }],
     date: new Date()
   })
   return insertedId.toString()
@@ -54,6 +54,22 @@ describe('SurveyResult MongoRepository', () => {
       const surveyResult = await sut.save(await makeFakeSaveSurveyResultData())
       expect(surveyResult).toBeTruthy()
       expect(surveyResult.id).toBeTruthy()
+      expect(surveyResult.answer).toBe('any_answer')
+    })
+
+    test('Should update survey result if its not new', async () => {
+      const accountId = await makeFakeAccountId()
+      const surveyId = await makeFakeSurveyId()
+      const { insertedId } = await surveyResultCollection.insertOne({
+        accountId: new ObjectId(accountId),
+        surveyId: new ObjectId(surveyId),
+        date: new Date(),
+        answer: 'other_answer'
+      })
+      const sut = makeSut()
+      const surveyResult = await sut.save({ accountId, surveyId, date: new Date(), answer: 'any_answer' })
+      expect(surveyResult).toBeTruthy()
+      expect(surveyResult.id).toBe(insertedId.toString())
       expect(surveyResult.answer).toBe('any_answer')
     })
   })
