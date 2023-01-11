f2fe542b60d34141d1c4f0537ae07b0013f46629
KelpGF <kelpgf@gmail.com>
2023-01-10 21:03:35 -0300

    test: ensure SaveSurveyResultRepository calls SaveSurveyResultRepository with correct values

---
+++ src/data/usescases/save-survey-result/db-save-survey-result.spec.ts
@@ -0,0 +1,50 @@
+import MockDate from 'mockdate'
+import { DbSaveSurveyResult } from './db-save-survey-result'
+import { SurveyResultModel, SaveSurveyResultModel, SaveSurveyResultRepository } from './db-save-survey-result-protocols'
+
+const makeFakeSaveSurveyResultData = (): SaveSurveyResultModel => ({
+  surveyId: 'any_survey',
+  accountId: 'any_account',
+  answer: 'any_answer',
+  date: new Date()
+})
+const makeFakeSurveyResult = (): SurveyResultModel => Object.assign({}, makeFakeSaveSurveyResultData(), { id: 'any_id' })
+
+const makeSaveSurveyResultRepositoryStub = (): SaveSurveyResultRepository => {
+  class SaveSurveyResultRepositoryStub implements SaveSurveyResultRepository {
+    async save (surveyResult: SaveSurveyResultModel): Promise<SurveyResultModel> {
+      return makeFakeSurveyResult()
+    }
+  }
+
+  return new SaveSurveyResultRepositoryStub()
+}
+
+type SutTypes = {
+  sut: DbSaveSurveyResult
+  saveSurveyResultRepositoryStub: SaveSurveyResultRepository
+}
+const makeSut = (): SutTypes => {
+  const saveSurveyResultRepositoryStub = makeSaveSurveyResultRepositoryStub()
+  const sut = new DbSaveSurveyResult(saveSurveyResultRepositoryStub)
+
+  return { sut, saveSurveyResultRepositoryStub }
+}
+
+describe('DbSaveSurveyResult', () => {
+  beforeAll(() => {
+    MockDate.set(new Date())
+  })
+
+  afterAll(() => {
+    MockDate.reset()
+  })
+
+  test('Should call SaveSurveyResultRepository with correct values', async () => {
+    const { sut, saveSurveyResultRepositoryStub } = makeSut()
+    const saveSpy = jest.spyOn(saveSurveyResultRepositoryStub, 'save')
+    const saveSurveyResultData = makeFakeSaveSurveyResultData()
+    await sut.save(saveSurveyResultData)
+    expect(saveSpy).toBeCalledWith(saveSurveyResultData)
+  })
+})
