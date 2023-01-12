55bf1b2454c0f87c7d6d9e9bf0831ec4024a908d
KelpGF <kelpgf@gmail.com>
2023-01-12 20:07:48 -0300

    test: ensure SaveSurveyResultController returns 500 if SaveSurveyResult throws

--- src/presentation/controller/survey-result/save-survey-result/save-survey-result-controller.spec.ts
+++ src/presentation/controller/survey-result/save-survey-result/save-survey-result-controller.spec.ts
@@ -118,4 +118,11 @@ describe('SaveSurveyResultController', () => {
       date: new Date()
     })
   })
+
+  test('Should return 500 if SaveSurveyResult throws', async () => {
+    const { sut, saveSurveyResultStub } = makeSut()
+    jest.spyOn(saveSurveyResultStub, 'save').mockRejectedValueOnce(new Error())
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(internalServerError(new Error()))
+  })
 })
