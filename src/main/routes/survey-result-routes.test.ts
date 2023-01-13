b9e1a67949055b939f93032114f4ff944e73ad11
KelpGF <kelpgf@gmail.com>
2023-01-13 18:46:04 -0300

    test: ensure save survey result returns 403 on save survey result without accessToken

---
+++ src/main/routes/survey-result-routes.test.ts
@@ -0,0 +1,14 @@
+
+import request from 'supertest'
+import app from '../config/app'
+
+describe('Survey Routes', () => {
+  describe('PUT /surveys/:surveyId/results', () => {
+    test('Should return 403 on save survey result without accessToken', async () => {
+      await request(app)
+        .put('/api/surveys/any_id/results')
+        .send({ answer: 'any_answer' })
+        .expect(403)
+    })
+  })
+})
