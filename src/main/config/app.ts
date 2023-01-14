4968d6fb4e48e66b9c014f46b2bd9c5ce58bb448
KelpGF <kelpgf@gmail.com>
2023-01-14 18:46:32 -0300

    feat: add swagger api

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,8 +1,10 @@
 import express from 'express'
+import setupSwagger from './config-swagger'
 import setupMiddleware from './middlewares'
 import setupRoutes from './routes'
 
 const app = express()
+setupSwagger(app)
 setupMiddleware(app)
 setupRoutes(app)
 export default app
---
+++ src/main/docs/index.ts
@@ -0,0 +1,8 @@
+export default {
+  openapi: '3.0.0',
+  info: {
+    title: 'Clean Node API',
+    description: 'API criada utilizando Clean Arq, SOLID e TDD',
+    version: '1.0.0'
+  }
+}
