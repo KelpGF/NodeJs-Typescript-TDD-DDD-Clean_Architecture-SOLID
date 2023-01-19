e5355fbd5171f41bc23dbd39f35ff58a06d20cbd
KelpGF <kelpgf@gmail.com>
2023-01-19 18:56:45 -0300

    test: ensure GetSurveyResultBySurveyId calls SearchSurveyById with correct surveyId

---
+++ src/presentation/controller/survey-result/get-survey-result-by-survey-id/get-survey-result-by-survey-id-controller.spec.ts
@@ -0,0 +1,30 @@
+import { SearchSurveyById } from '@/domain/usecases/survey/search-survey-by-id'
+import { mockSearchSurveyById } from '@/presentation/test'
+import { HttpRequest } from '@/presentation/protocols/http'
+import { GetSurveyResultBySurveyId } from './get-survey-result-by-survey-id-controller'
+
+const mockRequest = (): HttpRequest => ({
+  accountId: 'any_account_id',
+  params: { surveyId: 'any_survey_id' }
+})
+
+type SutTypes = {
+  sut: GetSurveyResultBySurveyId
+  searchSurveyByIdStub: SearchSurveyById
+}
+const makeSut = (): SutTypes => {
+  const searchSurveyByIdStub = mockSearchSurveyById()
+  const sut = new GetSurveyResultBySurveyId(searchSurveyByIdStub)
+
+  return { sut, searchSurveyByIdStub }
+}
+
+describe('GetSurveyResultBySurveyId Controller', () => {
+  test('Should call SearchSurveyById with correct surveyId', async () => {
+    const { sut, searchSurveyByIdStub } = makeSut()
+    const searchSpy = jest.spyOn(searchSurveyByIdStub, 'searchById')
+    const httpRequest = mockRequest()
+    await sut.handle(httpRequest)
+    expect(searchSpy).toHaveBeenCalledWith(httpRequest.params.surveyId)
+  })
+})
