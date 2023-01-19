c76d84348e4c6dea3e1a334aafc3b3bd8b205b4b
KelpGF <kelpgf@gmail.com>
2023-01-19 20:09:16 -0300

    chore: add dotenv

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,3 +1,6 @@
+import { config } from 'dotenv'
+config()
+
 export default {
   port: process.env.PORT || 5050,
   mongoConnectUrl: process.env.MONGO_CONNECT_URL || 'mongodb+srv://root:123@mongokelvin.5kpqp.mongodb.net/test',
