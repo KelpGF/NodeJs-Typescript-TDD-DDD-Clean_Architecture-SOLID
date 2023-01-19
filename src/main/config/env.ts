c2c03ed282fb2f4c28d23b3c15e450bac26cb4c9
KelpGF <kelpgf@gmail.com>
2023-01-19 20:11:15 -0300

    chore: update env

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -3,6 +3,6 @@ config()
 
 export default {
   port: process.env.PORT || 5050,
-  mongoConnectUrl: process.env.MONGO_CONNECT_URL || 'mongodb+srv://root:123@mongokelvin.5kpqp.mongodb.net/test',
+  mongoConnectUrl: process.env.MONGO_CONNECT_URL || 'mongodb://root:123@mongodb/test',
   jwtSecret: process.env.JWT_SECRET || '@(MD!@SEK#!qc'
 }
